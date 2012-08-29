58cab74285903b94ada4a6e9c885df8364eb5f58
Victor Dolirio <victor@victor-Inspiron-1545.(none)>
2012-08-29 07:49:38 -0300

    Melhoria no codigo

--- phantom_src/scrap_camara.js
+++ phantom_src/scrap_camara.js
@@ -4,16 +4,22 @@ page.onConsoleMessage = function(msg) {
     console.log(msg);
 };
 
-page.open(encodeURI("http://www2.camara.gov.br/deputados/pesquisa"), function (status) {
+var nomes = {
+    url: "http://www2.camara.gov.br/deputados/pesquisa",
+
+    buscaNomesDeDeputados: function(){
+	var list = document.querySelectorAll('select#deputado option');
+	for (var i = 0; i < list.length; ++i) {
+            console.log(list[i].value + ": " + list[i].innerHTML.replace(/<.*?>/g, ''));
+	}
+    }
+}
+
+page.open(encodeURI(nomes.url), function (status) {
     if (status !== "success") {
         console.log("Unable to access network");
     } else {
-        page.evaluate(function() {
-            var list = document.querySelectorAll('select#deputado option');
-            for (var i = 0; i < list.length; ++i) {
-                console.log(list[i].value + ": " + list[i].innerHTML.replace(/<.*?>/g, ''));
-            }
-        });
+        page.evaluate(nomes.buscaNomesDeDeputados);
     }
     phantom.exit();
 });
