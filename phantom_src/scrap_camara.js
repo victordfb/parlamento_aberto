a7a57c0476a4cfdd4c625188af1ebc1fb52e31dd
Victor Dolirio <victor@victor-Inspiron-1545.(none)>
2012-09-02 20:26:48 -0300

    Melhorias ao código

--- phantom_src/scrap_camara.js
+++ phantom_src/scrap_camara.js
@@ -1,25 +1,43 @@
-var page = require('webpage').create(), deputados;
-
-page.onConsoleMessage = function(msg) {
-    console.log(msg);
-};
-
-page.open(encodeURI("http://www2.camara.gov.br/deputados/pesquisa"), function (status) {
-    if (status !== "success") {
-        console.log("Unable to access network");
-    } else {
-        deputados = page.evaluate(function(){
-	    var list = document.querySelectorAll('select#deputado option');
-	    var dep = new Array(list.length);
-	    for(var i=0; i<list.length; i++){
-		dep[i] = list[i].value;
-	    }
-	    return dep;
-	});
-	for(var i = 0; i < deputados.length; ++i) {
-	    console.log(deputados[i]);
+function processPage(url, scraper, callback) {
+    var page = require('webpage').create(), deputados;
+    page.onConsoleMessage = function(msg) {
+	console.log(msg);
+    };
+    page.open(encodeURI(url), function (status) {
+	if (status !== "success") {
+            console.log("Unable to access network");
+	} else {
+	    callback(page.evaluate(scraper));
 	}
-    }
-    phantom.exit();
-});
+    });
+}
 
+processPage("http://www2.camara.gov.br/deputados/pesquisa",
+	    function(){
+		var list = document.querySelectorAll('select#deputado option');
+		var dep = new Array(list.length);
+		for(var i=0; i<list.length; i++){
+		    dep[i] = list[i].value;
+		}
+		return dep;
+	    },
+	    function(deputados){
+		for(var i = 0; i < deputados.length; ++i) {
+		    var dep_id = deputados[i].split("?")[1]
+		    if(dep_id === undefined)
+			continue;
+		    console.log(">>> "+dep_id);
+		    processPage("http://www.camara.gov.br/internet/Deputado/dep_Detalhe.asp?id=" + dep_id,
+				function(){
+				    var itens = document.querySelectorAll("#content>div>div>ul>li");
+				    for(var i=0; i<itens.length; i++) {
+					console.log(itens[i].innerHTML);
+				    }
+				    return itens;
+				},
+				function(deputado){
+				});
+		    if(i > 2)
+			break;
+		}
+	    });
